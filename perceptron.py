e5d321306f806e1272b326286a8f8446d3b45984
Diaa Essam <studywithdod@gmail.com>
2026-08-11 00:56:15 +0300

    Implement and test perceptron circuit

--- perceptron.py
+++ perceptron.py
@@ -1,19 +1,28 @@
-def perceptron(x, y, w1, w2, threshold):
-    total = w1 * x + w2 * y
+def perceptron(inputs, weights, threshold):
+    total = 0
+
+    for x, w in zip(inputs, weights):
+        total += x * w
 
     if total >= threshold:
         return 1
     return 0
 
+X = 1
+Y = 0
+Z = 1
+A = 0
 
-print(perceptron(0, 0, 1, 1, 2))  # 0
-print(perceptron(0, 1, 1, 1, 2))  # 0
-print(perceptron(1, 0, 1, 1, 2))  # 0
-print(perceptron(1, 1, 1, 1, 2))  # 1
+p1 = perceptron([X, Y], [1, 1], 2)
+p2 = perceptron([X, Z], [1, 1], 2)
+p3 = perceptron([Y, A], [-1, 1], 1)
+p4 = perceptron([X, Z, A], [1, 1, 1], 2)
 
-print('==============================')
+p5 = perceptron([p1, p2], [1, -1], 0)
+p6 = perceptron([p3, p4], [1, 1], 1)
 
-print(perceptron(0, 0, 1, -1, 0))
-print(perceptron(0, 1, 1, -1, 0))
-print(perceptron(1, 0, 1, -1, 0))
-print(perceptron(1, 1, 1, -1, 0))
+p7 = perceptron([p5, p6], [1, 1], 2)
+
+
+print(p1, p2, p3, p4, p5, p6, p7)
+print(p7)
