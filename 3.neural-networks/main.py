14236d4a345b8cb6ef92a63c771ce94bc1ec7745
Diaa Essam <studywithdod@gmail.com>
2026-09-08 14:00:00 +0300

    Set up for assignment 3

---
+++ 3.neural-networks/main.py
@@ -0,0 +1,12 @@
+# 6.Sigmoid Activation and Derivative
+def sigmoid(x):
+    """
+    :type x: float or numpy.ndarray
+    :rtype: same type as x
+    """
+
+def deriv_sigmoid(x):
+    """
+    :type x: float or numpy.ndarray
+    :rtype: same type as x
+    """
