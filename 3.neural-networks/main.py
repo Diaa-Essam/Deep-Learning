3435082b82a3c17695fd88a6324900a65bd0d907
Diaa Essam <studywithdod@gmail.com>
2026-09-15 17:15:12 +0300

    6. Sigmoid Activation and Derivative

--- 3.neural-networks/main.py
+++ 3.neural-networks/main.py
@@ -5,8 +5,13 @@ def sigmoid(x):
     :rtype: same type as x
     """
 
+    return 1 / (1 + np.exp(-x))
+
 def deriv_sigmoid(x):
     """
     :type x: float or numpy.ndarray
     :rtype: same type as x
     """
+
+    s = sigmoid()
+    return s - (1 - s)
