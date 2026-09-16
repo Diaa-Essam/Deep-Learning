80c4a451776529750e1137903f52b346834ae1c4
Diaa Essam <studywithdod@gmail.com>
2026-09-16 10:48:37 +0300

    7.Sigle Neuron Feedforward

--- 3.neural-networks/main.py
+++ 3.neural-networks/main.py
@@ -13,5 +13,30 @@ def deriv_sigmoid(x):
     :rtype: same type as x
     """
 
-    s = sigmoid()
-    return s - (1 - s)
+    s = sigmoid(x)
+    return s * (1 - s)
+
+# 7. Single Neuron Feedforward
+class Neuron:
+    def __init__(self, weights, bias):
+        """
+        :type weights: numpy.ndarray, shape (n_inputs,)
+        :type bias: float
+        """
+        self.weights = weights
+        self.bias = bias
+
+    def feedforward(self, inputs):
+        """
+        :type inputs: numpy.ndarray, shape (n_inputs,)
+        :rtype: float — the neuron's output after sigmoid activation
+        """
+
+        # z = np.dot(self.weights, inputs) + self.bias
+
+        z = 0
+        for i in range(len(self.weights)):
+            z += (inputs[i] * self.weights[i])
+        z += self.bias
+        
+        return sigmoid(z)
